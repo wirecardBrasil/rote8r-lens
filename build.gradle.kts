416cd70a3fd92553f7467d8292b1311cc6c4878d
elias.busato <elias.busato@wirecard.com>
2019-04-26 16:36:39 -0300

    fix embed mongo version

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,10 @@ repositories {
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile("org.jetbrains.kotlin:kotlin-reflect")
-    compile( "org.springframework.boot:spring-boot-starter-web:${springBootVersion}")
+    compile("org.springframework.boot:spring-boot-starter-web:${springBootVersion}")
     compile("br.com.moip.rote8r:core:1.0.2")
     compile("br.com.moip.rote8r:mongo-repository:1.0.2")
-    compile("de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.2.1-SNAPSHOT")
+    compile("de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.2.0")
 
     testCompile("junit", "junit", "4.12")
 }
